82d37d32e5e92c53808011a6d2259b5e2c75e07c
ioverpi <kellon08@gmail.com>
2023-03-31 22:48:39 -0600

    Put in some code to open a database.

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-let stateButton = null;
+const stateButton = document.querySelector("#stateButton");
+let currDbName = null;
+let currDb = null;
 
 function init(){
-    stateButton = document.getElementById("stateButton");
+    currDbName = localStorage.getItem("currDbName");
+    if(currDbName) openDatabase(currDbName);
+
+
     stateButton.addEventListener("click", function(){
         alert("hi");
     }, false);
 }
 
+function openDatabase(dbName){
+    const request = indexedDB.open(dbName);
+    request.addEventListener("error", function(event){
+        console.log("Error occurred when opening database.")
+        console.log(event);
+    });
+    request.addEventListener("success", function(event){
+        currDb = this.result;
+    });
+}
 
 window.addEventListener("load", init, false);
